0ce2a31f24d6f941aca6b9db3bed7b8bdfee009e
Luis Arias <luis@balsamiq.com>
2015-08-18 17:29:10 +0200

    Bump release and rest client builder plugin versions to recommended.

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -42,8 +42,8 @@ grails.project.dependency.resolution = {
     plugins {
         runtime ':asset-pipeline:2.5.0'
 
-        build(":release:3.0.1",
-              ":rest-client-builder:2.0.3") {
+        build(":release:3.1.1",
+              ":rest-client-builder:2.1.1") {
             export = false
         }
     }
